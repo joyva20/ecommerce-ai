67591d3495b99d968cc964a3f162cdc6cef19dff
navidzare7775 <navidzare7775@gmail.com>
2025-03-01 08:22:11 +0330

    [Fixed Build Bug]

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { assets } from "../assets/assets";
-import style from "./footer.module.css";
+import style from "./Footer.module.css";
 
 const Footer = () => {
   return (
